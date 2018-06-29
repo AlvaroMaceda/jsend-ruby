4cfc60769fa00e3348f5ada7f4a29221c8ebd5aa
Alvaro Maceda <alvaro@alvaromaceda.es>
2018-06-29 17:46:37 +0200

    Copied code from original project

--- lib/jsend.rb
+++ lib/jsend.rb
@@ -1,5 +1,117 @@
-require "jsend/version"
+# frozen_string_literal: true
 
-module Jsend
-  # Your code goes here...
+require "jsend/version"
+require 'json'
+
+module JSend
+
+  SUCCESS = 'success'
+  ERROR = 'error'
+  FAIL = 'fail'
+
+  def parse(json)
+    values = JSON.parse(json)
+    Response.class_for(values['status']).send(:from_hash, values)
+  end
+
+  def success(data)
+    JSend::SucessResponse.new(data)
+  end
+
+  def fail(error_data)
+    JSend::FailResponse.new(error_data)
+  end
+
+  def error(msg, code = nil, data = nil)
+    JSend::ErrorResponse.new(data, msg, code)
+  end
+
+  module_function :parse, :success, :fail, :error
+
+  class Response
+
+    def self.class_for(type)
+      case type
+      when JSend::SUCCESS
+        SucessResponse
+      when JSend::ERROR
+        ErrorResponse
+      when JSend::FAIL
+        FailResponse
+      else
+        raise 'Unsupported type of message'
+      end
+    end
+
+    def to_json
+      data = {status: @status, message: @message, code: @code, data: @data}
+      relevant_data = data.keep_if { |_, v| !v.nil? }
+      relevant_data.to_json
+    end
+
+    def success?
+      @status == JSend::SUCCESS
+    end
+
+    def fail?
+      @status == JSend::FAIL
+    end
+
+    def error?
+      @status == JSend::ERROR
+    end
+
+  end
+
+  private
+
+  class SucessResponse < Response
+
+    attr_reader :data
+
+    def initialize(data)
+      @status = JSend::SUCCESS
+      @data = data
+    end
+
+    def self.from_hash(hash)
+      puts 'From hash de success'
+    end
+
+  end
+
+  class FailResponse < Response
+
+    attr_reader :data
+
+    def initialize(data)
+      @status = JSend::FAIL
+      @data = data
+    end
+
+    def self.from_hash(hash)
+      puts 'From hash de success'
+    end
+
+  end
+
+  class ErrorResponse < Response
+
+    attr_reader :data, :message, :code
+
+    def initialize(data = nil, msg = nil, code = nil)
+      @status = JSend::ERROR
+      @data = data
+      @message = msg
+      @code = code
+    end
+
+    def self.from_hash(hash)
+      puts 'From hash de success'
+    end
+
+  end
+
+
+
 end
--- spec/jsend_spec.rb
+++ spec/jsend_spec.rb
@@ -1,11 +1,189 @@
 require "spec_helper"
 
 describe Jsend do
+
   it "has a version number" do
     expect(Jsend::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:data) {
+    {
+        item1: 'itema',
+        item2: 'itemb',
+        item3: {foo: 'bar', tee: 'poo'},
+        item4: ['asereje', 2, 45]
+    }
+  }
+
+  describe '#sucess' do
+
+      it 'Creates a message' do
+        jsend_message = JSend.success(data)
+
+        expect(jsend_message.success?).to be true
+        expect(jsend_message.error?).to be false
+        expect(jsend_message.fail?).to be false
+        expect(jsend_message.data).to eq(data)
+      end
+
+      it 'Exports to JSON' do
+        expected_json = {
+            status: JSend::SUCCESS,
+            data: data
+        }.to_json
+
+        generated_json = JSend.success(data).to_json
+
+        expect(generated_json).to eq(expected_json)
+      end
+
   end
+
+  describe '#fail' do
+
+    it 'creates a message' do
+      jsend_message = JSend.fail(data)
+
+      expect(jsend_message.fail?).to be true
+      expect(jsend_message.success?).to be false
+      expect(jsend_message.error?).to be false
+      expect(jsend_message.data).to eq(data)
+    end
+
+    it 'Exports to JSON' do
+      expected_json = {
+          status: JSend::FAIL,
+          data: data
+      }.to_json
+
+      generated_json = JSend.fail(data).to_json
+
+      expect(generated_json).to eq(expected_json)
+    end
+
+  end
+
+  describe '#error' do
+
+    it 'creates a message with error message only' do
+      error_message = 'Something failed'
+
+      jsend_message = JSend.error(error_message)
+
+      expect(jsend_message.error?).to be true
+      expect(jsend_message.success?).to be false
+      expect(jsend_message.fail?).to be false
+      expect(jsend_message.message).to equal(error_message)
+    end
+
+    it 'creates a message with code and data' do
+      error_message = 'Something failed very bad'
+      error_code = 999
+
+      jsend_message = JSend.error(error_message, error_code, data)
+
+      expect(jsend_message.error?).to be true
+      expect(jsend_message.message).to equal(error_message)
+      expect(jsend_message.code).to equal(error_code)
+      expect(jsend_message.data).to equal(data)
+    end
+
+    it 'Exports to JSON' do
+      error_message = 'The server has burnt in flames'
+      expected_json = {
+          status: JSend::ERROR,
+          message: error_message
+      }.to_json
+
+      generated_json = JSend.error(error_message).to_json
+
+      expect(generated_json).to eq(expected_json)
+    end
+
+    it 'Eports a complete error message to JSON' do
+      error_message = 'Your AWS instances let you in bankrupcy'
+      error_code = -987
+      expected_json = {
+          status: JSend::ERROR,
+          message: error_message,
+          code: error_code,
+          data: data
+      }.to_json
+
+      generated_json = JSend.error(error_message, error_code, data).to_json
+
+      expect(generated_json).to eq(expected_json)
+    end
+
+  end
+
+  describe 'Parser' do
+
+    context 'Success messages' do
+
+      xit 'Parses from JSON' do
+
+        message = {
+            status: JSend::SUCCESS,
+            data: data
+        }
+        json_to_parse = message.to_json
+        expected_data = JSON.parse(data.to_json)
+
+        jsend_message = JSend.parse(json_to_parse)
+
+        expect(jsend_message.success?).to be true
+        expect(jsend_message.data).to eq(expected_data)
+      end
+
+    end
+
+    context 'Failure messages' do
+
+      xit 'Parses from JSON' do
+
+        message = {
+            status: JSend::FAIL,
+            data: data
+        }
+        json_to_parse = message.to_json
+        expected_data = JSON.parse(data.to_json)
+
+        jsend_message = JSend.parse(json_to_parse)
+
+        expect(jsend_message.fail?).to be true
+        expect(jsend_message.data).to eq(expected_data)
+      end
+
+    end
+
+    context 'Error messages' do
+
+      xit 'Parses from JSON' do
+        error_message = 'Your AWS instances let you in bankrupcy'
+        error_code = -987
+
+        message = {
+            status: JSend::ERROR,
+            data: data,
+            code: error_code,
+            message: error_message
+        }
+        json_to_parse = message.to_json
+        expected_data = JSON.parse(data.to_json)
+
+        jsend_message = JSend.parse(json_to_parse)
+
+        expect(jsend_message.error?).to be true
+        expect(jsend_message.code).to eq(error_code)
+        expect(jsend_message.message).to eq(error_message)
+        expect(jsend_message.data).to eq(expected_data)
+
+      end
+
+    end
+
+  end
+
+
 end
